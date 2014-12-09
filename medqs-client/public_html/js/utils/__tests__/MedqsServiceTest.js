ae8474f576a32dbcc1dfc51d73ad70d09cfa9d3a
wonvincal <calvin.kl.wong@gmail.com>
2014-12-09 11:41:17 +0800

    First JEST test
    First JEST test

---
+++ medqs-client/public_html/js/utils/__tests__/MedqsServiceTest.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Calvin on 12/9/2014.
+ */
+// __tests__/MedqsServiceTest.js
+jest.dontMock('../MedqsService');
+
+describe('login', function(){
+   it('returns something', function(){
+
+   });
+});
