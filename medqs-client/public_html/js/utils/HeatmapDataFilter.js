e00bbb6667b94fff2cc29829c2662abd7ed02e98
wonvincal <calvin.kl.wong@gmail.com>
2015-03-01 23:11:36 +0800

    Decouple Heatmap from the rest of the application

---
+++ medqs-client/public_html/js/utils/HeatmapDataFilter.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Calvin on 2/28/2015.
+ */
+
+function HeatmapDataFilter(){
+
+}
+
+HeatmapDataFilter.prototype.init = function(){}
+
+HeatmapDataFilter.prototype.receiveAction = function(asof, actionType, data){
+    return [];
+}
---
+++ medqs-client/public_html/js/utils/HeatmapLink.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Calvin on 2/28/2015.
+ */
+function Link(){
+    this.startTime = null;
+    this.endTime = null;
+    this.id = null;
+    this.actionOnClick = null;
+}
